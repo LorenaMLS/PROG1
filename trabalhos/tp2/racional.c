fcaf51d7eaf398f13881deac26d76bcfa1b8bd73
LORENA MOREIRA LEITE DOS SANTOS <lmls22@inf.ufpr.br>
2024-10-01 17:26:12 -0300

    [add] tp2 função divide

---
+++ trabalhos/tp2/racional.c
@@ -0,0 +1,66 @@
+/* 
+ * Tipos Abstratos de Dados - TADs
+ * Arquivo de implementação para TAD racional.
+ * Feito em 20/09/2024 para a disciplina CI1001 - Programação 1.
+ *
+ * Este arquivo deve conter as implementações das funções cujos protótipos
+ * foram definidos em racional.h. Neste arquivo também podem ser definidas
+ * funções auxiliares para facilitar a implementação daquelas funções.
+*/
+
+/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
+#include <stdio.h>
+#include "racional.h"
+
+/*
+ * Implemente aqui as funcoes definidas no racionais.h; caso precise,
+ * pode definir aqui funcoes auxiliares adicionais, que devem ser usadas
+ * somente neste arquivo.
+*/
+
+/* retorna um número aleatório entre min e max, inclusive. */
+long aleat (long min, long max)
+{
+  /* implemente aqui */
+}
+
+/* Máximo Divisor Comum entre a e b      */
+/* calcula o MDC pelo método de Euclides */
+long mdc (long a, long b)
+{
+  /* implemente aqui */
+}
+
+/* Mínimo Múltiplo Comum entre a e b */
+/* mmc = (a * b) / mdc (a, b)        */
+long mmc (long a, long b)
+{
+  /* implemente aqui */
+}
+
+/* Recebe um número racional e o simplifica.
+ * Por exemplo, ao receber 10/8 deve retornar 5/4.
+ * Se ambos numerador e denominador forem negativos, deve retornar um positivo.
+ * Se o denominador for negativo, o sinal deve migrar para o numerador.
+ * Se r for inválido, devolve-o sem simplificar. */
+struct racional simplifica_r (struct racional r)
+{
+  /* implemente aqui */
+}
+
+/* implemente as demais funções de racional.h aqui */
+
+
+int divide_r (struct racional r1, struct racional r2, struct racional *r3)
+{
+  struct racional aux;
+
+  if (!r3 || !valido_r(r2))
+    return 0;
+
+  /*inverte o segundo racional*/
+  aux.num = r2.den;
+  aux.den = r2.num;
+
+  return multiplica_r(r1, aux, r3);
+}
