3ccf3c8f33449eac5944eea38772d2949db36628
LORENA MOREIRA LEITE DOS SANTOS <lmls22@inf.ufpr.br>
2024-10-30 17:08:08 -0300

    [add]funções lista.c

--- trabalhos/tp4/lista.c
+++ trabalhos/tp4/lista.c
@@ -8,13 +8,13 @@
 #include <stdlib.h>
 #include "lista.h"
 
-struct lista_t *lista_cria ()
+struct lista_t *lista_cria()
 {
     struct lista_t *lst;
 
     if (!(lst = malloc(sizeof(struct lista_t))))
         return NULL;
-    
+
     lst->prim = NULL;
     lst->ult = NULL;
     lst->tamanho = 0;
@@ -22,43 +22,126 @@ struct lista_t *lista_cria ()
     return lst;
 }
 
-struct lista_t *lista_destroi (struct lista_t *lst)
+/*função que cria um item, retorna o item ou NUll se erro*/
+struct item_t *item_cria(int valor)
 {
-    struct lista_t *aux;
+    struct item_t *novo_item;
 
-    while (lst->prim != NULL)
+    if (!(novo_item = malloc(sizeof(struct item_t))))
+        return NULL;
+
+    novo_item->ant = NULL;
+    novo_item->prox = NULL;
+    novo_item->valor = valor;
+
+    return novo_item;
+}
+
+/*função que destroi o item*/
+void item_destroi(struct item_t *item)
+{
+    if (!item)
+        return;
+
+    free(item);
+    item = NULL;
+}
+
+struct lista_t *lista_destroi(struct lista_t *lst)
+{
+    if (!lst)
+        return NULL;
+
+    /*deixa a lista somente com um elemento*/
+    while (lst->prim != lst->ult)
     {
-        aux->prim = lst->prim;
-        lst->prim = lst->prim->prox;
-        free (aux);
-        lst->tamanho--;
+        lst->ult = lst->ult->ant;
+        item_destroi(lst->ult->prox);
     }
-
-    lst->ult = NULL;
+    item_destroi(lst->prim);
     free(lst);
     lst = NULL;
 
-    return NULL; /*não tenho ctz*/
+    return lst;
 }
 
-int lista_insere (struct lista_t *lst, int item, int pos)
+int lista_insere(struct lista_t *lst, int item, int pos)
 {
-    
-}
+    struct item_t *novo_item, *aux;
+    int i;
 
-int lista_retira (struct lista_t *lst, int *item, int pos)
-{
-    struct lista_t *aux;
+    if (!lst)
+        return -1;
 
-    if (pos == -1)
+    if (!(novo_item = item_cria(item)))
+        return -1;
+
+    /*se a lista estiver vazia*/
+    if (!lst->prim)
     {
-
+        lst->prim = novo_item;
+        lst->ult = novo_item;
     }
+    /*insere no fim da lista*/
+    else if (pos < 0 || pos > lst->tamanho)
+    {
+        novo_item->ant = lst->ult;
+        lst->ult->prox = novo_item;
+        lst->ult = novo_item;
+    }
+    /*insere conforme a posição dada*/
     else
     {
-        aux = lista_procura(lst, item);
-        aux->ult = aux->ult->prox->prox;
-        free(aux);
-        lst->tamanho--;
+        aux = lst->prim;
+        for (i = 0; i < pos; i++)
+            aux = aux->prox;
+
+        novo_item->prox = aux;
+        novo_item->ant = aux->ant;
+
+        /*a ordem importa*/
+        aux->ant->prox = novo_item;
+        aux->ant = novo_item;
     }
+
+    return (lst->tamanho + 1);
+}
+
+int lista_retira(struct lista_t *lst, int *item, int pos)
+{
+    struct item_t *aux;
+    int i;
+
+    if (!lst && !item)
+        return -1;
+    /*remove se tem um elemento*/
+    if (lst->tamanho == 1)
+    {
+        item_destroi(lst->prim);
+        lst->prim = NULL;
+        lst->ult = NULL;
+    }
+    /*remove no fim da lista*/
+    else if (pos < 0 || pos > lst->tamanho)
+    {
+        /*aponto o lst->ult pro anterior e destruo ultimo*/
+        lst->ult = lst->ult->ant;
+        item_destroi(lst->ult->prox);
+        lst->ult->prox = NULL;
+    }
+    /*remove conforme posição dada*/
+    else
+    {
+        aux = lst->prim;
+        for (i = 0; i < pos; i++)
+            aux = aux->prox;
+
+        /*reapontamentos de itens*/
+        aux->ant->prox = aux->prox;
+        aux->prox->ant = aux->ant;
+
+        item_destroi(aux);
+    }
+
+    return (lst->tamanho - 1);
 }
