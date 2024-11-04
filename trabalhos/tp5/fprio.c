a79a5ec1ebf4910162541b1e55c44a76877c1788
LORENA MOREIRA LEITE DOS SANTOS <lmls22@inf.ufpr.br>
2024-11-04 11:48:52 -0300

    [add]destroi

--- trabalhos/tp5/fprio.c
+++ trabalhos/tp5/fprio.c
@@ -7,30 +7,71 @@
 #include <stdlib.h>
 #include "fprio.h"
 
-struct fprio_t *fprio_cria ()
+struct fprio_t *fprio_cria()
 {
     struct fprio_t *fila;
 
-    if(!(fila = malloc(sizeof(struct fprio_t))))
+    if (!(fila = malloc(sizeof(struct fprio_t))))
         return NULL;
-    
+
     fila->prim = NULL;
     fila->num = 0;
 
     return fila;
 }
 
-struct fpnodo_t *fpnodo_cria ()
+struct fpnodo_t *fpnodo_cria()
 {
     struct fpnodo_t *nodo;
 
-    if(!(nodo = malloc(sizeof(struct fpnodo_t))))
+    if (!(nodo = malloc(sizeof(struct fpnodo_t))))
         return NULL;
-    
+
     nodo->prox = NULL;
     nodo->item = NULL;
     nodo->prio = 0;
     nodo->tipo = 0;
 
     return nodo;
+}
+
+struct fpnodo_t *fpnodo_destroi(struct fpnodo_t *nodo)
+{
+    if (!nodo)
+        return NULL;
+
+    free(nodo->item);
+    free(nodo);
+    nodo = NULL;
+
+    return NULL;
+}
+
+struct fprio_t *fprio_destroi(struct fprio_t *f)
+{
+    struct fpnodo_t *aux, *aux2;
+
+    if (!f)
+        return NULL;
+
+    else if (f->num == 1)
+        fprio_destroi(f->prim);
+    else if (f->num > 1)
+    {
+        aux = f->prim;
+        aux2 = f->prim->prox;
+
+        while (aux2 != NULL)
+        {
+            fprio_destroi(aux);
+            aux = aux2;
+            aux2 = aux2->prox;
+        }
+        fprio_destroi(aux);
+    }
+
+    free(f);
+    f = NULL;
+
+    return NULL;
 }
